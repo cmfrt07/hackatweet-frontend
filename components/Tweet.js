08e1ab6af6e734de546c92a2c8910855c869c52e
cam.frt07@gmail.com <99878415+6oldenBerry@users.noreply.github.com>
2023-02-09 17:44:42 +0100

    commit reducer

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,15 +1,40 @@
 import styles from '../styles/Tweet.module.css';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTweet, removeTweet } from '../reducers/tweet';
 
 function Tweets() {
+
+  const dispatch = useDispatch();
+  const[str, setStr] = useState('')
+  const tweetArray = useSelector((state) => state.tweet.value);
+  console.log(tweetArray)
+
+  const handleCreateTweet = () => {
+    console.log("hello")
+    fetch('http://localhost:3000/tweet', {
+      method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ content: str}),
+    }).then(response => response.json())
+    .then(data => {
+      if(data.result) {
+        console.log(data)
+        dispatch(addTweet({tweet: data.tweet}))
+        setStr('')
+      }
+    });
+  }
+  
   return (
     <div>
       <main className={styles.main}>
         <h1>Home</h1>
         <div className={styles.tweetBox}>
-          <input placeholder="What's up?"></input>
+          <input maxLength="280" placeholder="What's up?" onChange={(e) => setStr(e.target.value)} value={str}></input>
           <div className={styles.charLength}>
-            <p>0/280</p>
-            <button className={styles.btnTweet}>Tweet</button>
+            <p>{str.length}/280</p>
+            <button className={styles.btnTweet} onClick={() => handleCreateTweet()}>Tweet</button>
           </div>
         </div>
       </main>
